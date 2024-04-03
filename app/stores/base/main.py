49c1b7e6d248ce084d4e4cbdb1f11fef6cc6c004
jinyang628 <chenjinyang4192@gmail.com>
2024-04-02 23:21:36 -0700

    Update

--- app/stores/base/main.py
+++ app/stores/base/main.py
@@ -1,14 +1,42 @@
-# # Use this script to do testing/one time operations on the database
+# Use this script to do testing/one time operations on the database
 
-# from app.stores.base.object import ObjectStore
-# import logging
+from app.stores.base.object import ObjectStore
+import logging
 
-# log = logging.getLogger(__name__)
+log = logging.getLogger(__name__)
 
-# def main():
-#     obj_store = ObjectStore("inference")
-#     obj_store.add_column("inference", "practice", "TEXT")
+def main():
+    obj_store = ObjectStore("user")
+    obj_store.execute(sql="""
+CREATE TRIGGER user_delete
+AFTER
+DELETE
+    ON user FOR EACH ROW BEGIN
+INSERT INTO
+    log (
+        table_name,
+        row_id,
+        data,
+        change_type
+    )
+VALUES
+    (
+        'user',
+        OLD.id,
+        json_object(
+            'version', OLD.version,
+            'name', OLD.name,
+            'email', OLD.email,
+            'api_key', OLD.api_key,
+            'usage', OLD.usage
+        ),
+        'DELETE'
+    );
+
+END;
+    """)
 
 
-# if __name__ == "__main__":
-#     main()
+
+if __name__ == "__main__":
+    main()
